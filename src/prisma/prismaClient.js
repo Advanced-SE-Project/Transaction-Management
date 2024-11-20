1a342fe94a8d9eb304941cb01dab7a65c78a99c2
mariniou77 <antonis.marinidis7@gmail.com>
2024-11-20 22:32:28 +0100

    update logging at prismaClient.js

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -2,6 +2,11 @@ const { PrismaClient } = require('@prisma/client');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Debugging environment variables
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("DATABASE_URL_TEST:", process.env.DATABASE_URL_TEST);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 // Choose the correct database URL based on NODE_ENV
 const databaseUrl = process.env.NODE_ENV === 'test'
     ? process.env.DATABASE_URL_TEST
