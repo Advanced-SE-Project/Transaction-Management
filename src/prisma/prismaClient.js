45042f578fc2adef321e1c57ebb89cc5308906bd
mariniou77 <antonis.marinidis7@gmail.com>
2024-11-20 22:25:29 +0100

    update logging at prismaClient.js

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -2,6 +2,11 @@ const { PrismaClient } = require('@prisma/client');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Choose the correct database URL based on NODE_ENV
+const databaseUrl = process.env.NODE_ENV === 'test'
+    ? process.env.DATABASE_URL_TEST
+    : process.env.DATABASE_URL;
+
 if (!databaseUrl) {
     console.error("DATABASE_URL is undefined. Check your environment variables.");
     throw new Error("DATABASE_URL is undefined.");
@@ -9,11 +14,6 @@ if (!databaseUrl) {
 
 // Log for debugging
 console.log("Resolved DATABASE_URL:", databaseUrl);
-
-// Choose the correct database URL based on NODE_ENV
-const databaseUrl = process.env.NODE_ENV === 'test'
-    ? process.env.DATABASE_URL_TEST
-    : process.env.DATABASE_URL;
 
 // Initialize PrismaClient with the selected database URL
 const prisma = new PrismaClient({
